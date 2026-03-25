b340aea8c6bcb654cf0ddb4047595d9854e91185
whgkdwnd <whgkdwnd@naver.com>
2026-03-25 14:35:27 +0900

    bug/학번입력

--- xv6edk2/xv6/student_id.c
+++ xv6edk2/xv6/student_id.c
@@ -1,7 +1,7 @@
 #include "types.h"
 #include "user.h"
 int main(int argc, char *argv[]) {
-int id=2026; // Your student id.
-printf(1,“My ID is %d\n“, id);
-exit();
+    int id=202201537; // Your student id.
+    printf(1, "My ID is %d\n", id);
+    exit();
 }
