62b86b0e526b8da222657ce3cbdfaa4af3eda755
whgkdwnd <whgkdwnd@naver.com>
2026-03-25 14:29:12 +0900

    bug/defs.h void to int

--- xv6edk2/xv6/defs.h
+++ xv6edk2/xv6/defs.h
@@ -121,7 +121,7 @@ int             wait(void);
 void            wakeup(void*);
 void            yield(void);
 void            exit2(int);
-void            wait2(int*);
+int            wait2(int*);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
