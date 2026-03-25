a02a2f3c09a6e4fad47970f50696b30222cc4bc3
whgkdwnd <whgkdwnd@naver.com>
2026-03-25 14:37:04 +0900

    bug/ "수정

--- xv6edk2/xv6/test_wait.c
+++ xv6edk2/xv6/test_wait.c
@@ -7,15 +7,15 @@ int main(int argc, char *argv[])
 	pid = fork();
         switch(pid)	{
 		case -1:
-			printf(2,“fork failed”);
+			printf(2,"fork failed");
 			exit2(0);
 		case 0:
-			message = “This is the child”;
+			message = "This is the child";
 			n = 5;
 			exit_code = 37;
 			break;
 		default:
-			message = “This is the parent”;
+			message = "This is the parent";
 			n = 3;
 			exit_code = 0;
 			break;
@@ -27,8 +27,8 @@ int main(int argc, char *argv[])
 
 	if (pid != 0) {
 		child_pid = wait2(&status);
-		printf(1, “Child has finished: PID = %d\n”, child_pid);
-		printf(1, “Child exited with code %d\n”, status);
+		printf(1, "Child has finished: PID = %d\n", child_pid);
+		printf(1, "Child exited with code %d\n", status);
 	}
 	exit2(exit_code);
 }
